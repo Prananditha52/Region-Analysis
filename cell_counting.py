9743943fbec89019bf1eaf46a00df224e5eb603b
Prananditha52 <90266599+Prananditha52@users.noreply.github.com>
2022-08-25 23:47:21 -0500

    Update cell_counting.py

--- cell_counting.py
+++ cell_counting.py
@@ -5,10 +5,7 @@ class CellCounting:
         pass
 
     def blob_coloring(self, image):
-        """Uses the blob coloring algorithm based on 5 pixel cross window and assigns region names
-        takes a input:
-        image: binary image
-        return: a list/dict of regions"""
+
         regions = dict()
         count = 1
         
@@ -45,14 +42,9 @@ class CellCounting:
         print(regions)
         return regions
     def compute_statistics(self, region):
-        """Computes cell statistics area and location
-        takes as input
-        region: list regions and corresponding pixels
-        returns: stats"""
 
-        # Please print your region statistics to stdout
-        # <region number>: <location or center>, <area>
-        # print(stats)
+
+
         stats = region
         for key, value in list(region.items()):
             area = len(value)
@@ -64,12 +56,6 @@ class CellCounting:
             # print(stats)
         return stats
     def mark_image_regions(self, image, stats):
-        """Creates a new image with computed stats
-        Make a copy of the image on which you can write text. 
-        takes as input
-        image: Input binary image
-        stats: stats regarding location and area
-        returns: image marked with center and area"""
         comp_image = image
         # comp_image = np.uint8(255 - image)
 
